ede1fec837f35fa20cf347b705d23f8296b3d0b8
Zdenko Gega <zgega@vub.hr>
2025-06-03 19:51:32 +0200

    Izrada scene za dodavanje igre

--- src/main/java/hr/faks/videogametracker/app/Main.java
+++ src/main/java/hr/faks/videogametracker/app/Main.java
@@ -26,22 +26,5 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch();
-
-/*        // ZA TESTIRANJE
-        List<Igra> igre = new ArrayList<>();
-
-        PcIgra pcIgra1 = new PcIgra("Cyberpunk 2077", "PC", "RPG", "2020-12-10", true, true, true, "specc", true) {};
-
-        PcIgra pcIgra2 = new PcIgra("The Witcher 3", "PC", "RPG", "2015-05-19", true, true, true, "specifikacija", false) {};
-
-        KonzolnaIgra konzolnaIgra1 = new KonzolnaIgra("God of War", "Playstation", "Action", "2018-04-20", true, false, true, "PS4") {};
-
-        igre.add(pcIgra1);
-        igre.add(pcIgra2);
-        igre.add(konzolnaIgra1);
-        // Ispis svih igara
-        for (Igra igra : igre) {
-            System.out.println(igra);
-        }*/
     }
 }
