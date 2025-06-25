85fb493d02680fb4fe3c8eacfca6768375566b21
zgega <zgega@vub.hr>
2025-06-25 02:17:13 +0200

    Napravljene osnovne CRUD operacije.

--- src/main/java/hr/faks/videogametracker/gui/DodajIgruController.java
+++ src/main/java/hr/faks/videogametracker/gui/DodajIgruController.java
@@ -1,26 +1,144 @@
 package hr.faks.videogametracker.gui;
 
+import hr.faks.videogametracker.model.Igra;
+import hr.faks.videogametracker.model.KonzolnaIgra;
+import hr.faks.videogametracker.model.PcIgra;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class DodajIgruController {
+
+    private MainController mainController;
+    private Igra igraZaUredivanje; // Igra koja se uređuje (null ako je nova igra)
+    private boolean uredivanjeMod = false;
 
     @FXML
     private Button btnSpremi;
 
     @FXML
-    private MenuButton drpDnPlatforma;
+    private Button btnOdustani;
 
     @FXML
-    private MenuButton drpDnZanr;
+    private ComboBox<String> cbPlatforma;
 
     @FXML
-    private DatePicker dtPickDatumIzlaska;
+    private TextField tfNaslov;
 
     @FXML
-    private TextField txtInputNaslov;
+    private DatePicker dpDatumIzlaska;
 
+    @FXML
+    private TextField tfZanr;
+
+    public void initialize() {
+        // Inicijalizacija platformi
+        cbPlatforma.getItems().addAll("PC", "Playstation", "Xbox", "Nintendo Switch");
+
+        // Postavljanje današnjeg datuma kao default
+        dpDatumIzlaska.setValue(LocalDate.now());
+
+        // Event handler za gumb spremi
+        btnSpremi.setOnAction(event -> spremiIgru());
+
+        // Event handler za gumb odustani
+        btnOdustani.setOnAction(event -> zatvoriProzor());
+    }
+
+    /**
+     * Postavlja kontroler u mod za uređivanje postojeće igre
+     * @param igra Igra koja se uređuje
+     */
+    public void postaviIgruZaUredivanje(Igra igra) {
+        if (igra != null) {
+            this.igraZaUredivanje = igra;
+            this.uredivanjeMod = true;
+
+            // Popuni polja s podacima igre
+            tfNaslov.setText(igra.getNaslovIgre());
+            tfZanr.setText(igra.getZanrIgre());
+            cbPlatforma.setValue(igra.getPlatforma());
+
+            // Postavi datum
+            try {
+                LocalDate datum = LocalDate.parse(igra.getDatumIzlaska(), DateTimeFormatter.ISO_DATE);
+                dpDatumIzlaska.setValue(datum);
+            } catch (Exception e) {
+                // U slučaju nepravilnog formata datuma, koristi današnji datum
+                dpDatumIzlaska.setValue(LocalDate.now());
+            }
+        }
+    }
+
+    private void spremiIgru() {
+        String naslov = tfNaslov.getText();
+        String platforma = cbPlatforma.getValue();
+        String zanr = tfZanr.getText();
+        LocalDate datumIzlaska = dpDatumIzlaska.getValue();
+
+        // Provjera jesu li sva polja popunjena
+        if (!naslov.isEmpty() && platforma != null && !zanr.isEmpty() && datumIzlaska != null) {
+            // Formatiranje datuma u željeni format (npr. YYYY-MM-DD)
+            String formatiranDatum = datumIzlaska.format(DateTimeFormatter.ISO_DATE);
+
+            if (uredivanjeMod && igraZaUredivanje != null) {
+                // Ažuriranje postojeće igre
+                igraZaUredivanje.setNaslovIgre(naslov);
+                igraZaUredivanje.setPlatforma(platforma);
+                igraZaUredivanje.setZanrIgre(zanr);
+                igraZaUredivanje.setDatumIzlaska(formatiranDatum);
+
+                if (mainController != null) {
+                    mainController.azurirajIgru();
+                    System.out.println("Ažurirana igra: " + naslov + " (datum izlaska: " + formatiranDatum + ")");
+                }
+            } else {
+                // Stvaranje nove igre
+                Igra novaIgra;
+
+                if (platforma.equalsIgnoreCase("PC")) {
+                    novaIgra = new PcIgra(naslov, platforma, zanr, formatiranDatum, true, true, true, "Standardne specifikacije", false);
+                } else {
+                    novaIgra = new KonzolnaIgra(naslov, platforma, zanr, formatiranDatum, true, false, true, platforma);
+                }
+
+                // Dodaj igru u listu u MainController-u
+                if (mainController != null) {
+                    mainController.dodajIgru(novaIgra);
+                    System.out.println("Dodana nova igra: " + naslov + " (datum izlaska: " + formatiranDatum + ")");
+                } else {
+                    System.err.println("MainController nije postavljen!");
+                }
+            }
+
+            // Zatvori prozor
+            zatvoriProzor();
+        } else {
+            // Prikaži upozorenje ako nisu sva polja popunjena
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("Upozorenje");
+            alert.setHeaderText("Nepotpuni podaci");
+            alert.setContentText("Molimo popunite sva polja!");
+            alert.showAndWait();
+
+            System.err.println("Sva polja moraju biti popunjena!");
+        }
+    }
+
+    private void zatvoriProzor() {
+        Stage stage = (Stage) btnSpremi.getScene().getWindow();
+        stage.close();
+    }
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
 }
--- src/main/java/hr/faks/videogametracker/gui/MainController.java
+++ src/main/java/hr/faks/videogametracker/gui/MainController.java
@@ -1,21 +1,28 @@
 package hr.faks.videogametracker.gui;
 
 import hr.faks.videogametracker.model.Igra;
-import hr.faks.videogametracker.model.PcIgra;
 import hr.faks.videogametracker.model.KonzolnaIgra;
+import hr.faks.videogametracker.model.PcIgra;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
     private ObservableList<Igra> listaIgara;
 
-    // TODO: Implementirati logiku glavnog kontrolera GUI aplikacije
-    /* ------------------------------ TABLE VIEW ------------------------------ */
     @FXML
     private Button btnDodaj;
 
@@ -26,35 +33,116 @@ public class MainController {
     private Button btnUredi;
 
     @FXML
-    private TableColumn<?, ?> colGodina;
+    private TableColumn<Igra, String> colNaslov;
 
     @FXML
-    private TableColumn<?, ?> colNaslov;
+    private TableColumn<Igra, String> colPlatforma;
 
     @FXML
-    private TableColumn<?, ?> colPlatforma;
+    private TableColumn<Igra, String> colZanr;
 
     @FXML
-    private TableColumn<?, ?> colZanr;
+    private TableColumn<Igra, String> colGodina;
 
     @FXML
     private Label lblUkupno;
 
     @FXML
-    private TableView<?> tableViewIgre;
+    private TableView<Igra> tableViewIgre;
 
     public void initialize() {
         // Spajanje stupaca s atributima
-        colNaziv.setCellValueFactory(new javafx.scene.control.cell.PropertyValueFactory<>("naslovIgre"));
-        colPlatforma.setCellValueFactory(new javafx.scene.control.cell.PropertyValueFactory<>("platforma"));
-        colZanr.setCellValueFactory(new javafx.scene.control.cell.PropertyValueFactory<>("zanrIgre"));
-        colDatumIzlaska.setCellValueFactory(new javafx.scene.control.cell.PropertyValueFactory<>("datumIzlaska"));
+        colNaslov.setCellValueFactory(new PropertyValueFactory<>("naslovIgre"));
+        colPlatforma.setCellValueFactory(new PropertyValueFactory<>("platforma"));
+        colZanr.setCellValueFactory(new PropertyValueFactory<>("zanrIgre"));
+        colGodina.setCellValueFactory(new PropertyValueFactory<>("datumIzlaska"));
 
         // Punjenje tablice podacima
         ucitajTestnePodatke();
         tableViewIgre.setItems(listaIgara);
 
+        // Postavljanje broja igara u label
+        azurirajBrojIgara();
+
+        // Postavljanje event handlera za gumbe
+        postaviEventHandlere();
+
         System.out.println("Broj igara: " + listaIgara.size());
+    }
+
+    private void postaviEventHandlere() {
+        btnDodaj.setOnAction(event -> otvoriDodajIgruProzor());
+        btnUredi.setOnAction(event -> urediIgru());
+        btnObrisi.setOnAction(event -> obrisiIgru());
+    }
+
+    private void otvoriDodajIgruProzor() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/hr/faks/videogametracker/fxml/dodaj-igru.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = new Stage();
+            stage.setTitle("Dodaj novu igru");
+            stage.setScene(scene);
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            DodajIgruController controller = fxmlLoader.getController();
+            controller.setMainController(this);
+
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void urediIgru() {
+        Igra odabranaIgra = tableViewIgre.getSelectionModel().getSelectedItem();
+        if (odabranaIgra != null) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/hr/faks/videogametracker/fxml/dodaj-igru.fxml"));
+                Scene scene = new Scene(fxmlLoader.load());
+                Stage stage = new Stage();
+                stage.setTitle("Uredi igru");
+                stage.setScene(scene);
+                stage.initModality(Modality.APPLICATION_MODAL);
+
+                DodajIgruController controller = fxmlLoader.getController();
+                controller.setMainController(this);
+                controller.postaviIgruZaUredivanje(odabranaIgra);
+
+                stage.showAndWait();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // Nema odabrane igre, prikaži poruku
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Informacija");
+            alert.setHeaderText("Nije odabrana igra");
+            alert.setContentText("Molimo odaberite igru koju želite urediti.");
+            alert.showAndWait();
+        }
+    }
+
+    private void obrisiIgru() {
+        Igra odabranaIgra = tableViewIgre.getSelectionModel().getSelectedItem();
+        if (odabranaIgra != null) {
+            listaIgara.remove(odabranaIgra);
+            azurirajBrojIgara();
+        }
+    }
+
+    public void dodajIgru(Igra igra) {
+        listaIgara.add(igra);
+        azurirajBrojIgara();
+    }
+
+    public void azurirajIgru() {
+        // Obavijesti TableView da su podaci promijenjeni
+        tableViewIgre.refresh();
+    }
+
+    private void azurirajBrojIgara() {
+        lblUkupno.setText(String.valueOf(listaIgara.size()));
     }
 
     // Metoda sa podacima za testiranje
@@ -62,9 +150,9 @@ public class MainController {
         // Testni podaci
         listaIgara = FXCollections.observableArrayList();
 
-        listaIgara.add(new PcIgra("Cyberpunk 2077", "PC", "RPG", "2020-12-10", true, true, true, "specifikacija", true) {});
-        listaIgara.add(new PcIgra("The Witcher 3", "PC", "RPG", "2015-05-19", true, true, true, "specifikacija", false) {});
-        listaIgara.add(new KonzolnaIgra("God of War", "Playstation", "Action", "2018-04-20", true, false, true, "PS4") {});
-        listaIgara.add(new KonzolnaIgra("Halo Infinite", "Xbox", "Shooter", "2021-12-08", true, false, true, "Xbox Series X") {});
+        listaIgara.add(new PcIgra("Cyberpunk 2077", "PC", "RPG", "2020-12-10", true, true, true, "specifikacija", true));
+        listaIgara.add(new PcIgra("The Witcher 3", "PC", "RPG", "2015-05-19", true, true, true, "specifikacija", false));
+        listaIgara.add(new KonzolnaIgra("God of War", "Playstation", "Action", "2018-04-20", true, false, true, "PS4"));
+        listaIgara.add(new KonzolnaIgra("Halo Infinite", "Xbox", "Shooter", "2021-12-08", true, false, true, "Xbox Series X"));
     }
 }
